9c2f7d5cf4f48dd88b1f61122338da7c76c3cccb
Gianluca <Gianluca@192.168.236.119>
2017-12-11 15:15:25 +0100

    APPLICATION CarteDiCreditoController

--- src/main/java/it/dstech/models/User.java
+++ src/main/java/it/dstech/models/User.java
@@ -2,17 +2,14 @@ package it.dstech.models;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
-
 public class User {
 
 	@Id
