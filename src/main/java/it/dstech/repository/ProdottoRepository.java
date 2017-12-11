992ecf9303999d802db393fba99b6f4354ed2a91
Home <Home@192.168.236.88>
2017-12-11 15:34:41 +0100

    Prodotto controller creato

--- src/main/java/it/dstech/repository/ProdottoRepository.java
+++ src/main/java/it/dstech/repository/ProdottoRepository.java
@@ -1,13 +1,18 @@
 package it.dstech.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import it.dstech.models.Categoria;
 import it.dstech.models.Prodotto;
 
 public interface ProdottoRepository extends CrudRepository<Prodotto, Integer> {
 
+	
 	Prodotto findById(int id);
 		
-	Prodotto findByCategoria (Categoria categoria);
+	List<Prodotto> findByCategoria (Categoria categoria);
+	
+	List<Prodotto> findByQuantitaDisponibileGreaterThan(double quantitaDisponibile);
 	
 }
