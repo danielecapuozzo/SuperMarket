678a9d95b4efbbb298dd105abd1e4ab887324cba
danielecapuozzo <danielecapuozzo@outlook.it>
2017-12-11 12:10:30 +0000

    Enum

---
+++ src/main/java/it/dstech/models/Categoria.java
@@ -0,0 +1,7 @@
+package it.dstech.models;
+
+public enum Categoria {
+	
+	ALIMENTI, PROD_CASA, PROD_PERSONA, ANIMALI
+
+}
---
+++ src/main/java/it/dstech/models/Unita.java
@@ -0,0 +1,7 @@
+package it.dstech.models;
+
+public enum Unita {
+
+	LITRO, CHILO, ETTO, PEZZO
+
+}
